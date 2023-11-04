c5372a62ec19256bfe5492f0da1d0c89bc30775f
UmerFaisal69 <149885078+UmerFaisal69@users.noreply.github.com>
2023-11-04 07:16:15 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const inquirer_1 = __importDefault(require("inquirer"));
+const answers = await inquirer_1.default.prompt([
+    {
+        type: "number",
+        name: "numberOne",
+        message: "Kindly Enter Your First No: "
+    },
+    {
+        type: "number",
+        name: "numberTwo",
+        message: "Kindly Enter Your Second No: "
+    },
+    {
+        type: "list",
+        name: "operator",
+        choices: ["*", "+", "-", "/"],
+        message: "Select Operator: "
+    },
+]);
+const { numberOne, numberTwo, operator } = answers;
+if (numberOne && numberTwo && operator) {
+    let result = 0;
+    if (operator === "+") {
+        result = numberOne + numberTwo;
+    }
+    else if (operator === "-") {
+        result = numberOne - numberTwo;
+    }
+    if (operator === "/") {
+        result = numberOne / numberTwo;
+    }
+    if (operator === "*") {
+        result = numberOne * numberTwo;
+    }
+    console.log("Your result is :", result);
+}
+else {
+    console.log("Kindly enter valid input");
+}
